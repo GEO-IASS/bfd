0dfbf17c2070431fabc6fffb3fce794bc0191b15
tpena <>
2004-12-03 10:57:04 +0000

    Modified bfdSaveData

--- matlab/bfdSaveData.m
+++ matlab/bfdSaveData.m
@@ -4,26 +4,17 @@ function bfdSaveData(kernelType, dataset, resultType, params, varargin)
   
 % BFD
   
-% Verifying which OS/Matlab version is being used
-if isequal(version, '6.1.0.450 (R12.1)')
-  result_path = ['/home/tpena/mlprojects/bfd/matlab/', ...
-                    resultType, '/'];
-  orig_path = '/home/tpena/mlprojects/bfd/matlab/';
-elseif isequal(version, '6.5.0.180913a (R13)')
-  result_path = ['H:\mlprojects\bfd\matlab\', resultType, '\'];
-  orig_path = 'H:\mlprojects\bfd\matlab';
-else
-  result_path = ['/home/tpena/mlprojects/bfd/matlab/', ...
-                    resultType, '/'];
-  orig_path = '/home/tpena/mlprojects/bfd/matlab/';
-end
+% Setting path to store results
+orig_path = pwd;
+result_path = dataset;
 
 % Going to recipient directory
 if isequal(resultType, 'partialResults')
-  if isequal(version, '6.1.0.450 (R12.1)')
-    cd([result_path, dataset]);
-  else
-    cd([result_path, dataset]);
+  [status, msg] = mkdir(result_path);
+  if status ~= 0  
+    cd(dataset);
+  else 
+    fprintf(['bfdSaveData: ', msg, '\n']);
   end
   dataset  = [dataset, datestr(clock,30)];
 else
@@ -35,7 +26,7 @@ if nargin == 4
     % Saving partial parameters
     save([kernelType{:} '_' dataset '_Error'], 'params');
   catch
-    fprintf('Error when saving, skipping...\n');
+    fprintf('bfdSaveData: Error when saving, skipping...\n');
   end  
 else
   error = varargin{1}; 
