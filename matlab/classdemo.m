49eb14dddce480d1d27ccc6f32ae123b4dae936d
tpena <>
2004-12-06 11:35:22 +0000

    Now saves data after training

--- matlab/classdemo.m
+++ matlab/classdemo.m
@@ -62,4 +62,4 @@ fprintf('Average error rate = %2.4f with std = %2.4f\n',...
          mean(error), std(error));
   
 % Uncomment this line if you want to save the final results
-%bfdSaveData(modSpecs.kernelType, dataset, 'UCIResults', params);
+bfdSaveData(modSpecs.kernelType, dataset, 'UCIResults', params);
