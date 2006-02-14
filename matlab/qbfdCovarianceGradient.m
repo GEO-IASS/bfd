0618c67748a2f2672c6043460b86799652598c42
tpena <>
2006-02-14 11:44:36 +0000

    Getting rid of old QBFD code, which was used for experimients with different precisions. The idea is that all remaining code in CVS should be related to BFD only, and in particular to revision 1.11

--- matlab/qbfdCovarianceGradient.m
+++
@@ -1,9 +0,0 @@
-function g = bfdCovarianceGradient(invK, postK)
-
-% BFDCOVARINCEGRADIENT Gradient of marginal log-likelihood wrt K
-
-% BFD
-
-% invK is the inverted Kernel
-g = -invK + invK*(postK)*invK;
-g = g*.5;
--- matlab/qbfdUpdateSigma.m
+++
@@ -1,7 +0,0 @@
-function model = qbfdUpdateSigma(model)
-
-% QBFDUPDATESIGMA Update the posterior for BFD
-
-% QBFD
-
-model.Sigma = pdinv(pdinv(model.kern.Kstore) + model.Lg);
